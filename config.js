45a2c712af1bc1c88322ca922d53d7fb2ed39458
sjborgman320-afk <sjborgman320@gmail.com>
2025-10-17 08:27:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+// config.js
+const API_CONFIG = {
+  endpoint: 'hhttps://clmrqyimovyudraksfze.supabase.co/rest/v1/life_decked_card_details_view?select=*',
+  headers: {
+    'APIKey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbXJxeWltb3Z5dWRyYWtzZnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NDcyNDMsImV4cCI6MjA0MzAyMzI0M30.1N7mOdA8CyiVL1HcBgR67Er53F109HckXpUXnpJfv2s'
+    //'Authorization': 'Bearer YOUR_API_KEY',
+    'Content-Type': 'application/json'
+  }
+};
